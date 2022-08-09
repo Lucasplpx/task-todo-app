910833148a4440001f3a8896095e3f3d5b7cb9ba
Lucas <lucasplpx@gmail.com>
2022-08-09 10:46:23 -0300

    component taskItem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,17 @@
 import { Header } from '../../components/Header';
 
-import { PlusCircle, ClipboardText } from 'phosphor-react';
+import { PlusCircle, ClipboardText, Trash } from 'phosphor-react';
 import {
   Container,
   NewTaskContainer,
   TaskInfoContainer,
+  TaskItem,
   TasksContainer,
   TasksCreated,
   TasksDone,
   TasksEmptyContainer,
   TodoContainer,
+  Wrapper,
 } from './styles';
 
 export function Home() {
@@ -27,20 +29,62 @@ export function Home() {
           <TaskInfoContainer>
             <TasksCreated>
               <p>Tarefas criadas</p>
-              <span>0</span>
+              <span>5</span>
             </TasksCreated>
 
             <TasksDone>
-              <p>Tarefas Concluídas</p>
-              <span>0</span>
+              <p>Concluídas</p>
+              <span>2 de 5</span>
             </TasksDone>
           </TaskInfoContainer>
 
-          <TasksEmptyContainer>
+          {/* <TasksEmptyContainer>
             <ClipboardText size={64} />
             <p>Você ainda não tem tarefas cadastradas</p>
             <p>Crie tarefas e organize seus itens a fazer</p>
-          </TasksEmptyContainer>
+          </TasksEmptyContainer> */}
+
+          <TaskItem>
+            <Wrapper>
+              <input type='checkbox' />
+            </Wrapper>
+
+            <p>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </p>
+            <button>
+              <Trash size={24} />
+            </button>
+          </TaskItem>
+
+          <TaskItem>
+            <Wrapper>
+              <input type='checkbox' />
+            </Wrapper>
+
+            <p>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </p>
+            <button>
+              <Trash size={24} />
+            </button>
+          </TaskItem>
+
+          <TaskItem isDone>
+            <Wrapper>
+              <input type='checkbox' checked />
+            </Wrapper>
+
+            <p>
+              Integer urna interdum massa libero auctor neque turpis turpis
+              semper. Duis vel sed fames integer.
+            </p>
+            <button>
+              <Trash size={24} />
+            </button>
+          </TaskItem>
         </TasksContainer>
       </TodoContainer>
     </Container>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -63,14 +63,16 @@ export const TasksContainer = styled.div`
 export const TaskInfoContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 1.5rem;
 `;
 
 const CountStyle = css`
-  width: 25px;
-  height: 19px;
+  min-width: 1.5rem;
+  height: 1.125rem;
+  padding: 0 8px;
 
   font-size: 0.75rem;
-  font-weight: bold;
+  font-weight: 700;
 
   line-height: 0.938rem;
 
@@ -88,6 +90,7 @@ export const TasksCreated = styled.div`
   gap: 8px;
 
   p {
+    font-weight: 700;
     font-size: 0.875rem;
     color: ${({ theme }) => theme['blue-100']};
   }
@@ -102,6 +105,7 @@ export const TasksDone = styled.div`
   gap: 8px;
 
   p {
+    font-weight: 700;
     font-size: 0.875rem;
     color: ${({ theme }) => theme['purple-100']};
   }
@@ -129,5 +133,115 @@ export const TasksEmptyContainer = styled.div`
 
   p:last-child {
     font-weight: 400;
+  }
+`;
+
+interface TaskItemProps {
+  isDone?: boolean;
+}
+
+export const TaskItem = styled.div<TaskItemProps>`
+  display: flex;
+  align-items: start;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 1rem;
+  margin-bottom: 12px;
+
+  border: 1px solid ${({ theme }) => theme['gray-400']};
+  border-radius: 8px;
+
+  ${({ isDone }) =>
+    isDone
+      ? css`
+          background: ${({ theme }) => theme['gray-500']};
+        `
+      : css`
+          background: ${({ theme }) => theme['gray-400']};
+        `}
+
+  p {
+    ${({ isDone }) =>
+      isDone &&
+      css`
+        text-decoration: line-through;
+      `}
+    font-size: 0.875rem;
+    line-height: 1.225rem;
+    max-width: 39.5rem;
+  }
+
+  button {
+    display: inline-block;
+    border: 0;
+    margin: 0;
+    padding: 0;
+    background: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+
+    &:focus {
+      display: flex;
+    }
+
+    &:hover {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      background: ${({ theme }) => theme['gray-500']};
+
+      width: 1.5rem;
+      height: 1.5rem;
+
+      border-radius: 1px;
+    }
+
+    svg {
+      cursor: pointer;
+      color: ${({ theme }) => theme['gray-300']};
+
+      &:hover {
+        color: ${({ theme }) => theme['red-300']};
+      }
+    }
+  }
+`;
+
+export const Wrapper = styled.div`
+  input[type='checkbox'] {
+    appearance: none;
+    -webkit-appearance: none;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+
+    border: 2px solid ${({ theme }) => theme['blue-100']};
+
+    :focus {
+      outline: 0;
+      box-shadow: 0 0 0 0px ${({ theme }) => theme['gray-100']};
+    }
+
+    &:hover {
+      background-color: ${({ theme }) => theme['blue-dark']};
+    }
+
+    &:checked {
+      content: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='14' height='14' fill='%23f0efef' viewBox='0 0 256 256'%3e%3crect width='14' height='14' fill='none'%3e%3c/rect%3e%3cpolyline points='216 72 104 184 48 128' fill='none' stroke='%23f0efef' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'%3e%3c/polyline%3e%3c/svg%3e");
+      background-color: ${({ theme }) => theme['purple-dark']};
+      border-color: ${({ theme }) => theme['purple-dark']};
+
+      &:hover {
+        background-color: ${({ theme }) => theme['purple-100']};
+        border-color: ${({ theme }) => theme['purple-100']};
+      }
+    }
   }
 `;
